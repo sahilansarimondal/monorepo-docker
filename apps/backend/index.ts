757292bd8c8076982747a6931d43346a5e584eab
Sahil Ansari Mondal <mondalrajesh761@gmail.com>
2025-04-09 11:10:41 +0530

    minor change to test the ci/cd

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello from backend!");
+    res.send("Hello from backend! Deployed on GCP");
 });
 
 app.get("/users", async (req, res) => {
